77b36330ee51be85f353f2766384839b4735588a
alekelbar <alekelbar.personal@gmail.com>
2023-10-26 22:17:10 -0600

    add method

--- include/BinarySearchTree.hpp
+++ include/BinarySearchTree.hpp
@@ -6,6 +6,25 @@ class BinarySearchTree
 private:
     BinaryNode<T> *root;
 
+    BinaryNode<T> *insert(BinaryNode<T> *node, const T &element)
+    {
+        if (node == nullptr)
+        {
+            return new BinaryNode<T>(element);
+        }
+
+        if (element < node->getElement())
+        {
+            node->setLeft(insert(node->getLeft(), element));
+        }
+        else if (element > node->getElement())
+        {
+            node->setRight(insert(node->getRight(), element));
+        }
+
+        return node;
+    }
+
 public:
     bool isEmpty() const { return root == nullptr; }
 
@@ -21,5 +40,10 @@ public:
     bool isInternal(BinaryNode<T> *node) const
     {
         return !isLeaf(node);
+    }
+
+    void add(const T &element)
+    {
+        root = insert(root, element);
     }
 };
